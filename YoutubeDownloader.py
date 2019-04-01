143543539f895d5b0d7c8613e7b455215e58f651
Vipul Gaur <vipulgaur2013@gmail.com>
2019-04-01 17:02:49 +0530

    Update YoutubeDownloader.py

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -6,14 +6,19 @@
 
 import pytube
 
+print("\n-----------------------------\nYOUTUBE VIDEO DOWNLOADER\n-----------------------------\n")
+
 link=input("Enter youtube URL here :>>")
 location=input("Enter Download Location: >>")
 location=location.replace("\\","\\")
 print("\n-------------------\nHold On...Processing Video\n-------------------\n")
-yt=pytube.YouTube(link)
-video=yt.streams.first()
+file=pytube.YouTube(link)
+video=file.streams.first()
 filename=video.default_filename
+print("\n-------------------\nFilename: ",filename ," \n-------------------\n")
 print("\n-------------------\nDownloading..!! Be Patient..!!\n-------------------\n")
 video.download(output_path=location, filename=filename)
 print("\n-------------------\nOolala..! Video Downloaded @", location,"\n-------------------\n")
+
+
 
